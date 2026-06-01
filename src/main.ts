3fa8fa0f73eaa1180ca863ddcaf7512047e6b3af
yassminn8954 <ens-yasminscotti@ugv.edu.br>
2026-05-31 23:45:12 -0300

    Enable CORS for local API tests in Hoppscotch

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
+  app.enableCors();
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
